39812081925ade8a5221ab93dbf612f262e4b451
Rishabh Rawat <rawatrishi1121@gmail.com>
2025-03-22 23:08:27 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+using namespace std;
+
+void ptrnOne(int line)
+{
+    //  * * * * * /
+    //  * * * * * /
+    //  * * * * * /
+    //  * * * * * /
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < line; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void ptrnTwo(int line)
+{
+    //  *         /
+    //  * *       /
+    //  * * *     /
+    //  * * * *  /
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void ptrnThree(int line)
+{
+    //  1         /
+    //  1 2       /
+    //  1 2 3     /
+    //  1 2 3 4   /
+
+    for (int i = 1; i <= line; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void ptrnFour(int line)
+
+{
+    //  1         /
+    //  2 2       /
+    //  3 3 3     /
+    //  4 4 4 4   /
+
+    for (int i = 1; i <= line; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i<< " ";
+        }
+        cout << endl;
+    }
+}
+
+void ptrnFive(int line)
+{
+    //  * * * *   /
+    //  * * *     /
+    //  * *       /
+    //  *         /
+
+    for (int i = 1; i <= line; i++)
+    {
+        for (int j = 4; j >= i; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void ptrnSix(int line)
+{
+    //  1 2 3 4   /
+    //  1 2 3     /
+    //  1 2       /
+    //  1         /
+
+    for (int i = 4; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j<<" ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the value for n ";
+    cin >> n;
+    ptrnOne(n);
+    ptrnTwo(n);
+    ptrnThree(n);
+    ptrnFour(n);
+    ptrnFive(n);
+    ptrnSix(n);
+
+    return 0;
+}
